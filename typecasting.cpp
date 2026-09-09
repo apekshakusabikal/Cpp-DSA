a7220966545600e580526ca44a2834b31ab3723a
Apeksha Kusabikal <apekshakusabikal@gmail.com>
2026-09-09 21:37:46 +0530

    Add files via upload

---
+++ typecasting.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num=10;
+    float b=34.5;
+    char a='A';
+    cout<<"the value of num when converted to float is: "<<(float)num;
+    cout<<"\nthe value of num when converted to float is: "<<float(num);
+    cout<<"\nthe value of b is: "<<int(b);
+    cout<<"\nthe value of b is: "<<(int)b;
+    cout<<"\nthe value A when converted integer: "<<int(a);
+
+
+    return 0;
+}
