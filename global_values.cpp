f576cc024f62ab7a1c8a7dfbf75a35451746dd08
Apeksha Kusabikal <apekshakusabikal@gmail.com>
2026-09-09 21:15:04 +0530

    Add files via upload

---
+++ global_values.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int c=56;
+int main()
+{
+    //GLOBAL VALUE
+    int a,b,c;
+    cout<<"\nEnter the value of a: ";
+    cin>>a;
+    cout<<"\nEnter the value of b: ";
+    cin>>b;
+    c=a+b;
+    cout<<"\nThe sum is: "<<c;
+    cout<<"\nThe global value c is: "<<::c;
+    return 0;
+
+}
