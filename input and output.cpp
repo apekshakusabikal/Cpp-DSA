0ca05b44711177aec3f336f7baf44c4653a669a8
Apeksha Kusabikal <apekshakusabikal@gmail.com>
2026-09-09 19:26:06 +0530

    Add files via upload

---
+++ input and output.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string name;
+    cout<<"\nEnter your name: ";
+    cin>>name;
+    cout<<"\n Hello "<<name;
+
+    //addition
+    int a=8,b=7;
+    cout<<"\nthe sum of the a+b is: "<<a+b;
+
+    //Student details
+    int age;
+    float marks;
+    cout<<"\nEnter age: ";
+    cin>> age;
+    cout<<"\nEnter marks: ";
+    cin>> marks;
+
+    int k=8,c=9,d=2;
+    cout<<"\nthe sum k+c+d: "<<k+c+d;
+    cout<<"\nthe average of k,c and d is: "<<(k+c+d)/3;
+
+    //
+    return 0;
+}
